e5a9954e78865ede6d5a2b748a254514486dd577
sunhao <2859310431@qq.com>
2021-04-12 18:30:37 +0800

    yigecuowu

--- main.cpp
+++ main.cpp
@@ -8,92 +8,90 @@ typedef int Vertex;
 typedef int WeightType;
 typedef char DataType;
 
-
-int  hanmingDistance(int v1,int v2)
 {
+int hanmingDistance(int v1, int v2); {
     {
-        int hanming = v1 ^v2;
-        int count = 0;
-        while (hanming != 0) {
-            hanming = hanming & (hanming - 1);
-            count++;
-        }
-        return count;
-    }  //计算汉明距离。
-
-    typedef struct ENode //定义边结构
-    {
-        Vertex v1, v2;
-        WeightType Weight;
-    } ENode, *PtrtoENode;
-    typedef PtrtoENode Edge;
-
-    typedef struct ADjVNode // 定义点结构
-    {
-        Vertex ADjV;
-        WeightType Weight;
-        ADjVNode *Next;
-    } ADjVNode, *PtrToAdjVNode;
-
-    typedef struct VNode // 顶点表头节点
-    {
-        PtrToAdjVNode FirstEdge; // 第一条边的地址 （一指针变量）
-        DataType Data;
-    } VNode, PtrToVNode, ADjList[MaxVertexNum];
-
-    typedef struct GNode  //图节点
-    {
-        int vexnum;  //顶点数
-        int arcnum;  //边数
-        ADjList G; // 顶点表
-    } GNode, *PtrToGNode;
-
-    typedef PtrToGNode LGraph;
-
-    LGraph CreateGraph;
-    int VexNum;  // 创建个图，并且定义顶点的编号。
-    {
-        Vertex V; // 定义图的最顶点节点V。
-        LGraph Graph; // 此图的存储方式为邻接表的存储图的图类型。
-
-        Graph->vexnum = VexNum; // 所有的节点都连在一起。
-        Graph->arcnum = 0; // 图的最外边的边数是0，也就是说所有的边都在图内。
-
-        for (V = 0, V < Graph->vexnum, V++) // 从最顶点节点开始循环探索，若小于图的的节点的数量，每次循环后编码加一。
         {
-            Graph->G[V].FirstEdge = NULL; //循环到最后一个节点的边和最顶点节点的边连在一起是结束，也就是围成一个圈。
-        }
-
-        return 0;
-
-    }
-
-    void InsertEdge(LGraph Graph, Edge E)// 插入边，规定该图所有的边为集合 E 。
-    {
-        PtrToAdjVNode NewNode;
+            int hanming = v1 ^v2;
+            int count = 0;
+            while (hanming != 0) {
+                hanming = hanming & (hanming - 1);
+                count++;
+            }
+            return count;
+        }  //计算汉明距离。
+        typedef struct ENode; //定义边结构
         {
-            NewNode = (PtrToAdjVNode) malloc(sizeof(ADjVNode));
-            NewNode->ADjV = E->v2;
-            NewNode->Weight = E->Weight; // 新节点的权重就是下一条边的权重
-            NewNode->Next = Graph->G[E->v1].FirstEdge; // 新节点的后继是图第一条边的后继。
-            Graph->G[E->v1].FirstEdge = NewNode;
-        }
+            Vertex v1, v2;
+            WeightType Weight;
+        } ENode, *PtrtoENode;
+        typedef PtrtoENode Edge;
 
-        // 这里指图的节点通过 有方向的边线 首尾连在一起。 （有向图）
-
+        typedef struct ADjVNode; // 定义点结构
         {
-            NewNode = (PtrToAdjVNode) malloc(sizeof(ADjVNode));
-            NewNode->ADjV = E->v1;
-            NewNode->Weight = E->Weight;
-            NewNode->Next = Graph->G[E->v2].FirstEdge;
-            Graph->G[E->v2].FirstEdge = NewNode;
+            Vertex ADjV;
+            WeightType Weight;
+            ADjVNode *Next;
+        } ADjVNode, *PtrToAdjVNode;
 
-            // 这个也是指图的节点通过 有方向的边线 首尾连在一起，和上面的结合，就是v1，v2两点都可以互通 （无向图）
+        typedef struct VNode; // 顶点表头节点
+        {
+            PtrToAdjVNode FirstEdge; // 第一条边的地址 （一指针变量）
+            DataType Data;
+        } VNode, PtrToVNode, ADjList[MaxVertexNum];
+
+        typedef struct GNode;  //图节点
+        {
+            int vexnum;  //顶点数
+            int arcnum;  //边数
+            ADjList G; // 顶点表
+        } GNode, *PtrToGNode;
+
+        typedef PtrToGNode LGraph;
+
+        LGraph CreateGraph(int VexNum) // 创建个图，并且定义顶点的编号。
+        {
+            Vertex V; // 定义图的最顶点节点V。
+            LGraph Graph; // 此图的存储方式为邻接表的存储图的图类型。
+
+            Graph->vexnum = VexNum; // 所有的节点都连在一起。
+            Graph->arcnum = 0; // 图的最外边的边数是0，也就是说所有的边都在图内。
+
+            for (V = 0; V < Graph->vexnum; V++) // 从最顶点节点开始循环探索，若小于图的的节点的数量，每次循环后编码加一。
+            {
+                Graph->G[V].FirstEdge = NULL; //循环到最后一个节点的边和最顶点节点的边连在一起是结束，也就是围成一个圈。
+            }
+
+            return Graph;
 
         }
 
+        void InsertEdge(LGraph Graph, Edge E);// 插入边，规定该图所有的边为集合 E 。
+        {
+            PtrToAdjVNode NewNode;
+            {
+                NewNode = (PtrToAdjVNode) malloc(sizeof(ADjVNode));
+                NewNode->ADjV = E->v2;
+                NewNode->Weight = E->Weight; // 新节点的权重就是下一条边的权重
+                NewNode->Next = Graph->G[E->v1].FirstEdge; // 新节点的后继是图第一条边的后继。
+                Graph->G[E->v1].FirstEdge = NewNode;
+            }
 
-        LGraph BuildGraph() // 构建以邻接表存储图的类型的图。
+            // 这里指图的节点通过 有方向的边线 首尾连在一起。 （有向图）
+
+            {
+                NewNode = (PtrToAdjVNode) malloc(sizeof(ADjVNode));
+                NewNode->ADjV = E->v1;
+                NewNode->Weight = E->Weight;
+                NewNode->Next = Graph->G[E->v2].FirstEdge;
+                Graph->G[E->v2].FirstEdge = NewNode;
+
+                // 这个也是指图的节点通过 有方向的边线 首尾连在一起，和上面的结合，就是v1，v2两点都可以互通 （无向图）
+
+            }
+        }
+
+        LGraph BuildGraph(); // 构建以邻接表存储图的类型的图。
         {
             LGraph Graph; // 构建图。
             Edge E;  //边
@@ -116,12 +114,19 @@ int  hanmingDistance(int v1,int v2)
                     printf(" v1 v2 Weight \n"); // 输出两个节点和权重。
                     InsertEdge(Graph, E); // 插入图的边
                 }
+                for (V=0;V<Graph->vexnum; V++)
+               {
+                scanf("%c",&(Graph->G[V].Data));
+               }
             }
+        }
 
             return Graph;
         };
 
-        void print(LGraph Graph) {
+
+        void Print(LGraph Graph);
+        {
             for (int i = 0; i < Graph->vexnum; i++) // 循环节点。
             {
                 int i;
@@ -141,7 +146,8 @@ int  hanmingDistance(int v1,int v2)
             }
         }
 
-        int main() {
+        int main();
+        {
             LGraph Graph;
             Graph = BuildGraph();
             print(Graph);
@@ -151,6 +157,7 @@ int  hanmingDistance(int v1,int v2)
             return 0;  //
         }
 
-
     }
+
 }
+
